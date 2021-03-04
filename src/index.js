28ca2e4a142f32aa270d2805fcf6fe9a9d2d0a52
annatopalidi <topalidisanna@gmail.com>
2021-03-04 19:10:44 +0100

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = str.length; i >= 0; i--) {
+        for (let k = 0; k < bracketsConfig.length; k++) {
+            while (str.includes(bracketsConfig[k].join(''))) {
+                str = str.replace(bracketsConfig[k].join(''), '');
+            }
+        }
+    }
+    return str === '' ? true : false;
 }
